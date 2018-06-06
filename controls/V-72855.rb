a0c2f129d9c029616143dc9698c1c05d34fdfed5
steve-hetzel <steve.hetzel@crunchydata.com>
2018-06-06 13:35:18 -0400

    Checking in updated revision controls with logic changes.

---
+++ controls/V-72855.rb
@@ -0,0 +1,131 @@
+# encoding: utf-8
+#
+=begin
+-----------------
+Benchmark: PostgreSQL 9.x Security Technical Implementation Guide
+Status: Accepted
+
+This Security Technical Implementation Guide is published as a tool to improve
+the security of Department of Defense (DoD) information systems. The
+requirements are derived from the National Institute of Standards and
+Technology (NIST) 800-53 and related documents. Comments or proposed revisions
+to this document should be sent via email to the following address:
+disa.stig_spt@mail.mil.
+
+Release Date: 2017-01-20
+Version: 1
+Publisher: DISA
+Source: STIG.DOD.MIL
+uri: http://iase.disa.mil
+-----------------
+=end
+
+PG_DBA = attribute(
+  'pg_dba',
+  description: 'The postgres DBA user to access the database',
+  default: 'stig_dba'
+)
+
+PG_DBA_PASSWORD = attribute(
+  'pg_dba_password',
+  description: "password for the postgres dba password",
+  default: 'stigD@1234#')
+
+PG_DB = attribute(
+  'pg_db',
+  description: 'the default postgres database',
+  default: 'stig_test_db'
+)
+
+PG_PORT = attribute(
+  'pg_port',
+  description: "The port that the postgres server is listening to",
+  default: '5432'
+)
+
+PG_HOST = attribute(
+  'pg_host',
+  description: "Hostname or ip allow to connect to the database",
+  default: '127.0.0.1'
+)
+
+only_if do
+  command('psql').exist?
+end
+
+
+control "V-72855" do
+  title "PostgreSQL must limit privileges to change functions and triggers, and
+  links to software external to PostgreSQL."
+  desc  "If the system were to allow any user to make changes to software
+  libraries, those changes might be implemented without undergoing the
+  appropriate testing and approvals that are part of a robust change management
+  process.  Accordingly, only qualified and authorized individuals must be
+  allowed to obtain access to information system components for purposes of
+  initiating changes, including upgrades and modifications.  nmanaged changes
+  that occur to the database code can lead to unauthorized or compromised
+  installations."
+  impact 0.5
+  tag "severity": "medium"
+  tag "gtitle": "SRG-APP-000133-DB-000179"
+  tag "gid": "V-72855"
+  tag "rid": "SV-87507r1_rule"
+  tag "stig_id": "PGS9-00-000710"
+  tag "cci": "CCI-001499"
+  tag "nist": ["CM-5 (6)", "Rev_4"]
+
+  tag "check": "Only owners of objects can change them. To view all functions,
+  triggers, and trigger procedures, their ownership and source, as the database
+  administrator (shown here as \"postgres\") run the following SQL:
+
+  $ sudo su - postgres
+  $ psql -x -c \"\\df+\"
+
+  Only the OS database owner user (shown here as \"postgres\") or a PostgreSQL
+  superuser can change links to external software.
+
+  As the database administrator (shown here as \"postgres\"), check the permissions
+  of configuration files for the database:
+
+  $ sudo su - postgres
+  $ ls -la ${PGDATA?}
+
+  If any files are not owned by the database owner or have permissions allowing
+  others to modify (write) configuration files, this is a finding."
+
+  tag "fix": "To change ownership of an object, as the database administrator
+  (shown here as \"postgres\"), run the following SQL:
+
+  $ sudo su – postgres\
+  $ psql -c \"ALTER FUNCTION function_name OWNER TO new_role_name\"
+
+  To change ownership of postgresql.conf, as the database administrator (shown
+  here as \"postgres\"), run the following commands:
+
+  $ sudo su - postgres
+  $ chown postgres:postgres ${PGDATA?}/postgresql.conf
+  $ chmod 0600 ${PGDATA?}/postgresql.conf
+
+  To remove superuser from a role, as the database administrator (shown here as
+  \"postgres\"), run the following SQL:
+
+  $ sudo su - postgres
+  $ psql -c \"ALTER ROLE rolename WITH NOSUPERUSER\""
+
+# @todo draft code below, how do we test for manual checks?
+
+  dbs = nil
+  db = nil
+
+  if !("#{PG_DB}".to_s.empty?)
+    db = ["#{PG_DB}"]
+    dbs = db.map { |x| "-d #{x}" }.join(' ')
+  end
+
+  sql = postgres_session(PG_DBA, PG_DBA_PASSWORD, PG_HOST)
+  
+  describe sql.query ("SELECT n.nspname as Schema, p.proname as Name, pg_catalog.pg_get_function_result(p.oid) as ResultDataType, pg_catalog.pg_get_function_arguments(p.oid) as ArgumentDataTypes, pg_catalog.pg_get_userbyid(p.proowner) as Owner, CASE WHEN prosecdef THEN 'definer' ELSE 'invoker' END AS Security, pg_catalog.array_to_string(p.proacl, E'\n') AS AccessPrivileges FROM pg_catalog.pg_proc p LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace LEFT JOIN pg_catalog.pg_language l ON l.oid = p.prolang WHERE pg_catalog.pg_function_is_visible(p.oid) AND n.nspname <> 'pg_catalog' AND n.nspname <> 'information_schema' AND pg_catalog.pg_get_userbyid(p.proowner) <> 'postgres';") do
+    its('output') { should eq '' } 
+  end
+
+end
