ad1db83528a96e968113c4c760c0870daba97eb0
Nhi Truong <ntruong@mitre.org>
2020-03-04 11:31:27 -0500

    Updated README

--- controls/V-72951.rb
+++ controls/V-72951.rb
@@ -159,20 +159,4 @@ control "V-72951" do
       its('output') { should match /ERROR:  permission denied for schema test_schema/ }
     end
 
-    describe command("grep 'SELECT \* FROM test_schema.test_table' $(find '#{pg_log_dir}' -type f -printf '%T@ %p\0' | sort -rz | sed -Ezn '1s/[^ ]* //p')") do
-      its('exit_status') { should eq 0 }
-    end
-
-    describe command("grep 'INSERT INTO test_schema.test_table VALUES (0)' $(find '#{pg_log_dir}' -type f -printf '%T@ %p\0' | sort -rz | sed -Ezn '1s/[^ ]* //p')") do
-      its('exit_status') { should eq 0 }
-    end
-
-    describe command("grep 'UPDATE test_schema.test_table SET id = 1 WHERE id = 0' $(find '#{pg_log_dir}' -type f -printf '%T@ %p\0' | sort -rz | sed -Ezn '1s/[^ ]* //p')") do
-      its('exit_status') { should eq 0 }
-    end
-
-    describe command("grep 'DROP TABLE test_schema.test_table' '$(find '#{pg_log_dir}' -type f -printf '%T@ %p\0' | sort -rz | sed -Ezn '1s/[^ ]* //p')") do
-      its('exit_status') { should eq 0 }
-    end
-
 end
