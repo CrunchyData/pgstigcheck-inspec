559870d67e5e29028d2517dafab368889ae7dc24
boussarhane <boussarhane@mitre.org>
2020-06-25 17:05:07 -0400

    Update V-72901.rb

--- controls/V-72901.rb
+++ controls/V-72901.rb
@@ -69,17 +69,16 @@ control "V-72901" do
       it { should be_grouped_into 'root' }
       its('mode') { should cmp '0755' }
     end
-  end  
-
-  if virtualization.system == 'docker'
-    describe "If any non-PostgreSQL software directories exist on the disk directory, examine or investigate their use." do
-      skip "If any of the directories are used by other applications, including third-party applications that use the PostgreSQL, this is a finding."
-    end
-    
-  else
-    describe command("lsof | awk '$9 ~ \"#{dir}\" {print $1}'") do
-      its('stdout') { should match /^$|postgres|postmaster/ }
-      its('stderr') { should eq '' }
-    end
+    if virtualization.system == 'docker'
+      describe "If any non-PostgreSQL software directories exist on the disk directory, examine or investigate their use." do
+        skip "If any of the directories are used by other applications, including third-party applications that use the PostgreSQL, this is a finding."
+      end
+      
+    else
+      describe command("lsof | awk '$9 ~ \"#{dir}\" {print $1}'") do
+        its('stdout') { should match /^$|postgres|postmaster/ }
+        its('stderr') { should eq '' }
+      end
+    end  
   end
 end
